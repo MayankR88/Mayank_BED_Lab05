054e68ed4a8d0d216e5e08b074fb90111beb5a9d
Mayank Rawat <110289559+MayankR88@users.noreply.github.com>
2023-01-10 13:28:10 +0530

    Add files via upload

---
+++ springboot-thymeleaf-webapp/src/test/java/com/greatlearning/ems/springbootthymeleafwebapp/SpringbootThymeleafWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatlearning.ems.springbootthymeleafwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
